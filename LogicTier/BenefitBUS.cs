f065d46488819a118bde1278de50dec1fb6eb837
Tanya203 <opisme3@gmail.com>
2024-03-25 15:09:32 +0700

    call api get put post benefit

---
+++ LogicTier/BenefitBUS.cs
@@ -0,0 +1,24 @@
+﻿using CLIENT.Models;
+using CLINET.DataTier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLINET.LogicTier
+{
+    public class BenefitBUS
+    {
+        private readonly BenefitDAL benefitDAL;
+
+        public BenefitBUS()
+        {
+            benefitDAL = new BenefitDAL();
+        }
+        public async Task<List<Benefit>> GetAllBenefits()
+        {
+            return await benefitDAL.GetAllBenefits();
+        }
+    }
+}
--- ViewModels/StaffWorkScheduleDetailViewModel.cs
+++ ViewModels/StaffWorkScheduleDetailViewModel.cs
@@ -8,6 +8,20 @@ namespace CLIENT.ViewModels
 {
     public class StaffWorkScheduleDetailViewModel
     {
+        public string WsId { get; set; }
 
+        public DateTime? WorkDate { get; set; }
+
+        public string StaffId { get; set; }
+
+        public string FullName { get; set; }
+
+        public string PositionName { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public bool? DateOff { get; set; }
+
+        public decimal? DayOff { get; set; }
     }
 }
